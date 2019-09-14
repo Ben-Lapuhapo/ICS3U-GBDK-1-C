8985af921d6917b9a08cc27318ce0231c51946d5
Ubuntu <ubuntu@ip-172-31-20-120.ec2.internal>
2019-09-14 01:19:49 +0000

    Added Hello world program for gameboy

---
+++ game.c
@@ -0,0 +1,15 @@
+//  Copyright (c) 2019 Ben Lapuhapo All Rights Reserved.
+//
+//  Created by: Ben Lapuhapo
+//  Created on: September 2019
+//  This is the "Hello, World!" program on gameboy
+
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+
+    return 0;
+}
